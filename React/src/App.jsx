29355d730bccd8919f271399c55ad1f0c0efc61c
Prabhat <pkjha187@gmail.com>
2023-07-25 21:37:26 +0530

    Setting up Landing page for Zyng

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -2,14 +2,15 @@ import './App.css';
 import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from "react-router-dom"
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import ZLanding from './pages/ZLanding';
 
 function App() {
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<Layout/>} >
       <Route index element={<Home/>} />
-      {/* <Route path="/About" element={<About/>} />
-      <Route path="/Contact" element={<Contact/>} />
+      <Route path="/zland" element={<ZLanding/>} />
+      {/* <Route path="/Contact" element={<Contact/>} />
       <Route path="/Setup/:setupid" element={<SingleSetup/>} /> */}
     </Route>
   ))
---
+++ React/src/pages/ZLanding.jsx
@@ -0,0 +1,16 @@
+import Footer from "../components/ZLanding/Footer"
+import Header from "../components/ZLanding/Header"
+
+const ZLanding = () => {
+  return (
+    <div>
+        <Header/>
+        <main>
+
+        </main>
+        <Footer/>
+    </div>
+  )
+}
+
+export default ZLanding
